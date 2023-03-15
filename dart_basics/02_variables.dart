07cf6b0fcdf91a3a911befdd17c8d7aac59625d3
Nadi Rahmat Endrawan <nadi.r.endrawan@smart-tbk.com>
2023-03-15 14:13:57 +0700

    First commit

---
+++ dart_basics/02_variables.dart
@@ -0,0 +1,11 @@
+void main() {
+  int outputInt = 2; // initialization
+
+  print("output: $outputInt");
+
+  late int lateOutputInt; // declaration
+
+  lateOutputInt = 42; // allocation
+
+  print('lateOutputInt: $lateOutputInt');
+}
---
+++ dart_basics/03_data_types.dart
@@ -0,0 +1,31 @@
+
+
+void main() {
+  bool boolOutput = false;
+  print(boolOutput);
+
+  int intOutput = -67;
+  print(intOutput);
+
+  double doubleOutput = 9.231;
+  print(doubleOutput);
+
+  String stringOutput = 'This is the value of stringOutput';
+  print(stringOutput);
+
+  // dynamic dynamicVar = 4.5;
+
+  // keywords:
+  late int exampleInt;
+  exampleInt = 1;
+  print(exampleInt);
+
+  final int finalInt = 3;
+  print(finalInt);
+
+  const int constInt = 4;
+  print(constInt);
+
+  var varOutput = "text";
+  print(varOutput);
+}
